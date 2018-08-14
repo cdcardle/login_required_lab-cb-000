5cd97467273592eea75215c2bffed8fbf8df778d
Chris Cardle <cdcardle@gmail.com>
2018-08-14 19:19:27 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:name].nil? || params[:name] == ""
+      recdirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
   end
 
   def destroy
