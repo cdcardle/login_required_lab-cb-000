da3769f0ed4355bbf04a6d04c52b71587d977989
Chris Cardle <cdcardle@gmail.com>
2018-08-14 18:27:40 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'application#home'
 
+  get '/login' => 'application#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
